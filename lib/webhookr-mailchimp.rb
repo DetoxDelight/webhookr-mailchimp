02e723e55d64c9f21b6e987878fe48f1118fc9c9
Michael Johann <mjohann@rails-experts.com>
2020-01-23 16:49:04 +0100

    Update webhookr-mailchimp.rb

--- lib/webhookr-mailchimp.rb
+++ lib/webhookr-mailchimp.rb
@@ -8,7 +8,6 @@ module Webhookr
     class Adapter
       SERVICE_NAME = 'mailchimp'
       EVENT_KEY = "type"
-      RENAMED_EVENT_KEY = "event_key"
       PAYLOAD_KEY = "data"
 
       include Webhookr::Services::Adapter::Base
@@ -21,7 +20,7 @@ module Webhookr
         Array.wrap(parse(raw_response)).collect do |p|
           Webhookr::AdapterResponse.new(
             SERVICE_NAME,
-            p.fetch(RENAMED_EVENT_KEY),
+            p.fetch(EVENT_KEY),
             OstructUtils.to_ostruct(p)
           )
         end
@@ -30,19 +29,10 @@ module Webhookr
       private
 
       def parse(raw_response)
-        convert_event_key_to_ruby_friendly_name(
-          assert_valid_packet(
-            Rack::Utils.parse_nested_query(raw_response)
-          )
-        )
+        assert_valid_packet(Rack::Utils.parse_nested_query(raw_response))
       end
 
-      def convert_event_key_to_ruby_friendly_name(packet)
-        # Ruby 1.8.7 fix for key 'type'
-        rename = { EVENT_KEY => RENAMED_EVENT_KEY }
-        Hash[packet.map {|k, v| [rename[k] || k, v] }]
-      end
-
+      
       def assert_valid_packet(parsed_response)
         raise(Webhookr::InvalidPayloadError,
               "Missing event key '#{EVENT_KEY}' in packet"
